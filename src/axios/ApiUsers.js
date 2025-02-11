d0203aea5f68ab4b489786da305a0586499c3741
Mauro Rafael Moya <mauro.7795@gmail.com>
2025-02-11 16:57:34 -0600

    ajustando campos

--- src/axios/ApiUsers.js
+++ src/axios/ApiUsers.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 /**Creamos la instancia de axios con la url del servidor */
 export const AxiosPapeleria = axios.create({
-    baseURL: "http://192.168.130.6:4000/api/"
+    baseURL: "http://192.168.100.28:4000/api/"
   });
