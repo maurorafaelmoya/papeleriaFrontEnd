92df3a4b8008c2f12907cd6e947749e8b22149e8
maurorafaelmoya <mauro.7795@gmail.com>
2025-02-12 17:52:20 -0600

    [FT-NAVEGATION] configurando el servicio externo de backend

--- src/axios/ApiUsers.js
+++ src/axios/ApiUsers.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 /**Creamos la instancia de axios con la url del servidor */
 export const AxiosPapeleria = axios.create({
-    baseURL: "http://localhost:4000/api/"
+    baseURL: "https://papeleriabackend.up.railway.app/api"
   });
